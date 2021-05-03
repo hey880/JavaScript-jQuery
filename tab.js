89f91ab55271b8f97bc47ba04f120e419e4c87d2
hey880 <hey880@naver.com>
2021-05-03 21:10:50 +0900

    Add tab menu using for loop

---
+++ tab.js
@@ -0,0 +1,8 @@
+for(let i=0; i<$('.tab-button').length; i++) {
+    $('.tab-button').eq(i).click(function(){
+        $('.tab-button').removeClass('active');
+        $('.tab-content').removeClass('show');
+        $('.tab-button').eq(i).addClass('active');
+        $('.tab-content').eq(i).addClass('show');
+    })
+}
